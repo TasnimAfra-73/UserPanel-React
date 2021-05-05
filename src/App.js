d4ccc9d7b3f071f7f208874ef9d890884cf54b20
ehsanrishat <erishat162079@bscse.uiu.ac.bd>
2021-05-06 04:29:34 +0600

    Updated add new modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Searchbar from "./components/SearchBar";
 import UserTable from "./components/UserTable";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   const [open, setOpen] = React.useState(false);
@@ -18,9 +19,9 @@ function App() {
       padding: theme.spacing(2, 4, 3),
     },
     modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
     },
   }));
 
@@ -38,15 +39,65 @@ function App() {
     <div>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Searchbar />
-        <Button style={{ backgroundColor: "green" }} onClick={handleOpen}>Add New</Button>
-        <Modal className ={classes.modal}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        <div className={classes.paper}>hiiiiiiii</div>
-      </Modal>
+        <Button style={{ backgroundColor: "green" }} onClick={handleOpen}>
+          Add New
+        </Button>
+        <Modal
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          {/* <div className={classes.paper}>hiiiiiiii</div> */}
+          <div className={classes.paper}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <h2>Add User</h2>
+              <a>X</a>
+            </div>
+            <div style={{ display: "flex" }}>
+              <form noValidate autoComplete="off">
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <label>First Name</label>
+                  <TextField
+                    id="outlined-search"
+                    label="Enter first name"
+                    type="search"
+                    variant="outlined"
+                  />
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <label>Last Name</label>
+
+                  <TextField
+                    id="outlined-search"
+                    label="Enter user name or email"
+                    type="search"
+                    variant="outlined"
+                  />
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <label>E-mail</label>
+
+                  <TextField
+                    id="outlined-search"
+                    label="Enter user name or email"
+                    type="search"
+                    variant="outlined"
+                  />
+                </div>
+                <div style={{ display: "flex", alignItems:"end" }}>
+                <button style={{ backgroundColor: "green" }} type="submit">
+                  Save
+                </button>
+                <button style={{ backgroundColor: "red" }} onClick={handleOpen}>
+                  Close
+                </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </Modal>
       </div>
       <UserTable />
     </div>
