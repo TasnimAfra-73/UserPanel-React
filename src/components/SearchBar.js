5f52213809d90db13894f7e3acd436594b85ae1b
ehsanrishat <erishat162079@bscse.uiu.ac.bd>
2021-05-09 14:43:46 +0600

    Search module implemented

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
@@ -11,14 +11,62 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SearchBar() {
+export default function SearchBar({ users, setUsers }) {
+  const [searchInput, setSearchInput] = useState("");
   const classes = useStyles();
+  const searchArray = JSON.parse(localStorage.getItem("users"));
+
+  const getUsers = () => {
+    let tempUsers = JSON.parse(localStorage.getItem("users"));
+    console.log("get users called");
+    if (!tempUsers) {
+      localStorage.setItem("users", JSON.stringify([]));
+      tempUsers = JSON.parse(localStorage.getItem("users"));
+    }
+    setUsers(tempUsers);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let tempUsers = [];
+    if (searchInput === "") {
+      getUsers();
+    }
+
+    searchArray.forEach((element) => {
+      var firstName = element.firstName;
+      var lastName = element.lastName;
+      var email = element.email;
+      if (
+        firstName.includes(searchInput) ||
+        lastName.includes(searchInput) ||
+        email.includes(searchInput)
+      ) {
+        tempUsers.push(element);
+        setUsers(tempUsers);
+        console.log(users);
+      }
+    });
+  };
 
   return (
-    <div style = {{display: "flex" }}>
+    <div style={{ display: "flex" }}>
       <Button style={{ backgroundColor: "green" }}>Search</Button>
-      <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-search" label="Enter user name or email" type="search" variant="outlined" />
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          id="outlined-search"
+          label="Search by user name or email"
+          type="search"
+          variant="outlined"
+          onChange={(event) => {
+            setSearchInput(event.target.value);
+          }}
+        />
       </form>
     </div>
   );
--- src/components/userTable.js
+++ src/components/userTable.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import EditForm from "./EditForm";
 
-export default function DataTable({ users, setUsers }) {
+export default function DataTable({ users, setUsers}) {
   console.log(users);
   const columns = [
     {
